89042a13ec224c80466e92580e76fe38b240a404
s-h-a-m-a-n <esergeev.home@gmail.com>
2023-04-13 02:22:26 +0300

    twoSum

---
+++ twoSum/twoSum_test.go
@@ -0,0 +1,28 @@
+package twoSum
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		ret    []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{3, 3}, 6, []int{0, 1}},
+	}
+
+	for i, tc := range tests {
+		tc := tc
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, twoSum(tc.nums, tc.target), tc.ret)
+		})
+	}
+}
